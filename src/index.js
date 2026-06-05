fd9a12a8158fe61b3c03b52b3252c8953ee0e799
krispaje <nork.kristina@gmail.com>
2026-06-05 19:55:10 +0300

    second correction UX

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function generateAnswer(event) {
   let userInputValue = userInputElement.value;
   let answerElement = document.querySelector("#AI-answer");
   answerElement.classList.remove("hidden");
-  answerElement.innerHTML = `Generating horror movie about ${userInputValue}....<span class="loader">⏳</span>`;
+  answerElement.innerHTML = `Searching for: ${userInputValue}....<span class="loader">⏳</span>`;
   apiKey = "9c764o4f1faeb3c3bct46f73f94b0e7b";
   context =
     "be like a real movie expert, who is really into horror movies, pick only one movie, it should be horror, be very precise, it can be a title or just a context of a movie, dont write any other words, only movie name, year and director, be polite, if there is no valid answer, just be funny and at the end tell them to give you more context";
