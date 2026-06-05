ffda4c65111f547cd049529053928115cdf89407
krispaje <nork.kristina@gmail.com>
2026-06-05 19:38:11 +0300

    Improved UX

--- src/index.js
+++ src/index.js
@@ -7,12 +7,17 @@ function typeAnswer(response) {
 function generateAnswer(event) {
   event.preventDefault();
 
-  let userInput = document.querySelector("#user-input");
+  let userInputElement = document.querySelector("#user-input");
+  let userInputValue = userInputElement.value;
+  let answerElement = document.querySelector("#AI-answer");
+  answerElement.classList.remove("hidden");
+  answerElement.innerHTML = `Generating horror movie about ${userInputValue}....<span class="loader">⏳</span>`;
   apiKey = "9c764o4f1faeb3c3bct46f73f94b0e7b";
   context =
-    "be like a real movie expert, who is really into horror movies, pick only one movie, it should be horror, be very precise, dont write any other words, only movie name, year and director, be polite";
-  prompt = `movie idea: ${userInput.value}`;
+    "be like a real movie expert, who is really into horror movies, pick only one movie, it should be horror, be very precise, it can be a title or just a context of a movie, dont write any other words, only movie name, year and director, be polite, if there is no valid answer, just be funny and at the end tell them to give you more context";
+  prompt = `movie idea: ${userInputValue}`;
   apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
   axios.get(apiURL).then(typeAnswer);
 }
 
